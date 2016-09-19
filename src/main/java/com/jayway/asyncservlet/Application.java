30764fbf0104b3c0f61382a6acccfb331022e1ad
willedwards <will.edwards@me.com>
2016-09-19 20:48:07 +0100

    tidy up

--- src/main/java/com/jayway/asyncservlet/Application.java
+++ src/main/java/com/jayway/asyncservlet/Application.java
@@ -1,12 +1,22 @@
 package com.jayway.asyncservlet;
 
+import com.jayway.asyncservlet.domain.RepoListService;
+import com.jayway.asyncservlet.github.GitHubRepoListService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @ComponentScan
+@Configuration
 @EnableAutoConfiguration
 public class Application {
+
+    @Bean
+    RepoListService buildRepoListService(){
+        return new GitHubRepoListService();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
